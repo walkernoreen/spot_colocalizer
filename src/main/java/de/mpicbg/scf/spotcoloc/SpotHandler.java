2a20e2a2cd0fced2fd55f92d1956e74b777410f2
walker <walker@extern.mpi-cbg.de>
2020-04-01 19:27:01 +0200

    refactor

--- src/main/java/de/mpicbg/scf/spotcoloc/SpotHandler.java
+++ src/main/java/de/mpicbg/scf/spotcoloc/SpotHandler.java
@@ -27,7 +27,7 @@ public class SpotHandler <T extends RealType<T>>{
      * Finds spots in a single channel of an image. For spot detection the trackmate LoG detector is used.
      * If the input image has a roi, then spot detection is restricted to this region.
      * @param imp image with at least one channel, 2D or 3D
-     * @param channel which channel to use. count starts at 1
+     * @param channelnr which channel to use. count starts at 1
      * @param radius_um Spot radius in um (for LoGDetector).
      * @param threshold Quality threshold for Log detector. `threshold` is first scaled with the heuristic
      *                 radius_um^3 (3D) or radius_um^2 (2D) before usage as cutoff in the LoGDetector.
@@ -36,99 +36,59 @@ public class SpotHandler <T extends RealType<T>>{
      * @param doSubpixel for LoG Detector
      * @param doMedian for LogDetector
      */
-    public List detectSpots(ImagePlus imp, int channel, double radius_um, double threshold, boolean doSubpixel,
+    public List detectSpots(ImagePlus imp, int channelnr, double radius_um, double threshold, boolean doSubpixel,
                             boolean doMedian) {
-        // (adapted from https://imagej.net/Scripting_TrackMate)
+        // initialize result
+        List<Spot> spots = new ArrayList<>();
 
-        // CAREFUL: Dimension handling in imglib2:
-        // Img/RandomAccessibleInterval dimension order is xyczt,
-        // but if a dimension is missing it is simply skipped(!): e.g. without c&t: xyz, without z&t: xyc
-        // therefore information on nchannels, nslices, .. is lost during wrapping
+        // extract single channel for spot detection
+        RandomAccessibleInterval<T> img = Utils.extractSingleChannelImg(imp, channelnr);
+        if (img==null) { return new ArrayList<Spot>();} // channel didn't exist
 
-
-        // TODO: move the channel extraction to a separate function?
-        int nChannels = imp.getNChannels();
-        int nSlices = imp.getNSlices();
-        int nFrames=imp.getNFrames();
-
-        // convert to imglib2 image
-        RandomAccessibleInterval<T> img = ImageJFunctions.wrapReal(imp); // TODO name rai?
-
-        // extract single channel
-        if (nChannels>1) {
-            if (channel <= nChannels) {
-                img = Views.hyperSlice(img, 2, channel - 1); // xyc(zt)
-            } else {
-                IJ.log("Error: channel " + channel + " does not exist.");
-                return new ArrayList<Spot>(); //TODO check this
-            }
-        }
-        else if (channel>1) {
-            IJ.log("Warning: Image has only single channel but a higher channel number was selected. Ignoring channel selection.");
-        }
-
-        // restrict to roi region (bounding box).
-        RandomAccessibleInterval<T> interval=img;
-
-        Roi roi = imp.getRoi();
-        if (roi!=null) {
-            Rectangle bounds = roi.getBounds();
-            long[] istart = new long[]{bounds.x, bounds.y, 0, 0}; // xy(zt). single channel
-            long[] iend = new long[]{bounds.x+bounds.width, bounds.y+bounds.height, nSlices - 1, nFrames - 1}; // xy(z,t)
-
-            interval = Views.interval(img, istart, iend);
-        }
+        // restrict channel to roi region (bounding box).
+        RandomAccessibleInterval<T> interval = Utils.restrictSingleChannelImgToRoiRect(img, imp);
 
 
-        // get calibration
+        // get image calibration
         Calibration calib = imp.getCalibration();
         double[] calibration = new double[] {calib.pixelWidth, calib.pixelHeight, calib.pixelDepth};
-        System.out.println("Image calibration: "+Arrays.toString(calibration));
 
-        // heuristic scaling of threshold to reduce sensitivity to the used radius. normalized to radius=1m
-        if (nSlices>1) {
+        // heuristic scaling of threshold to reference radius=1um
+        if (imp.getNSlices()>1) {
             threshold=threshold/(radius_um*radius_um*radius_um); // * or / anisotropy (in future?)?
         }
         else {
             threshold=threshold/(radius_um*radius_um);
         }
 
-        //	Setup spot detector (see http://javadoc.imagej.net/Fiji/fiji/plugin/trackmate/detection/LogDetector.html)
-        // LogDetector( RandomAccessible, Interval, ...) // blubb
-       // LogDetector detector = new LogDetector(img, img, calibration, radius_um, threshold, doSubpixel, doMedian);
-        LogDetector detector = new LogDetector(img, interval, calibration, radius_um, threshold, doSubpixel, doMedian);
-
-        // prepare final result
-        List<Spot> spots = new ArrayList<>();
 
         // == Detect the spots ==
-        if (detector.process()) {
-            // Get the array of spots found
-            List<Spot> spotsraw = detector.getResult();
+        LogDetector detector = new LogDetector(img, interval, calibration, radius_um, threshold, doSubpixel, doMedian);
 
-            IJ.log("\nSpot detection in Channel " + channel + " finished.");
+        if (detector.process()) {
+            // Get the list of detected spots
+            List<Spot> spotsraw = detector.getResult();
+            IJ.log("\nSpot detection in Channel " + channelnr + " finished.");
             IJ.log("Spot detection found " + spotsraw.size() + " spots (in full image, resp. roi bounding rectangle if roi exists.)");
 
             // Filter spots by ROI
+            Roi roi = imp.getRoi();
             if (roi == null) {
                 spots = spotsraw;
             } else {
-                // check if pixel position within roi
+                // check if spot pixel position within roi
                 double[] position;
-
                 for (Spot currentspot : spotsraw) {
-                    System.out.println(currentspot);
                     position = getPositionPx(currentspot, calib);
                     if (roi.contains((int) position[0], (int) position[1])) {
                         spots.add(currentspot);
                     }
                 }
-                IJ.log("Filter spots by ROI region: kept " + spots.size() + " spots.");
+                IJ.log("Filter spots by roi region: kept " + spots.size() + " spots.");
             }
         }
-        // otherwise:  detection failed
         else {
-            System.out.println("The detector could not process the data.");
+            IJ.log("The detector could not process the data.");
         }
 
         return spots;
@@ -143,7 +103,6 @@ public class SpotHandler <T extends RealType<T>>{
      */
     public static double[] getPositionPx(Spot spot, Calibration calib) {
         double[] position = new double[3];
-        System.out.println(spot.echo());
         position[0] = spot.getDoublePosition(0) / calib.pixelWidth;
         position[1] = spot.getDoublePosition(1) / calib.pixelHeight;
         position[2] = spot.getDoublePosition(2) / calib.pixelDepth;
--- src/main/java/de/mpicbg/scf/spotcoloc/Utils.java
+++ src/main/java/de/mpicbg/scf/spotcoloc/Utils.java
@@ -1,7 +1,77 @@
 package de.mpicbg.scf.spotcoloc;
 
+import fiji.plugin.trackmate.Spot;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.Roi;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.view.Views;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 /**
- * Statici utility functions
+ * Static utility functions
  */
 public class Utils {
+    /**
+     * Extracts a single channel of imp and returns it as imglib2 RandomAccessibleInterval img.
+     *
+     * @param imp       2D or 3D, single or multi-channel. single time point (multi-timepoint not tested!).
+     * @param channelnr channel number. counter starts at 1. If the input has single channel, channelnr is ignored.
+     * @return img: single channel img. or null if input was multichannel and the channelnr does not exist
+     */
+    public static <T extends RealType<T>> RandomAccessibleInterval<T> extractSingleChannelImg(ImagePlus imp, int channelnr) {
+
+        // CAREFUL: Dimension handling in imglib2:
+        // Img/RandomAccessibleInterval dimension order is xyczt,
+        // but if a dimension is missing it is simply skipped(!): e.g. without c&t: xyz, without z&t: xyc
+        // therefore information on nchannels, nslices, .. is lost during wrapping
+
+        int nChannels = imp.getNChannels();
+
+        // convert to imglib2 image
+        RandomAccessibleInterval<T> img = ImageJFunctions.wrapReal(imp);
+
+        // extract single channel
+        if (nChannels > 1) {
+            if (channelnr <= nChannels) {
+                img = Views.hyperSlice(img, 2, channelnr - 1); // xyc(zt)
+            } else {
+                IJ.log("Error: channel " + channelnr + " does not exist in multichannel image.");
+                return null;
+            }
+        } else if (channelnr > 1) {
+            IJ.log("Warning: Image has only single channel but a higher channel number was selected. Ignoring channel selection.");
+        }
+
+        return img;
+    }
+
+
+    /**
+     * Crops img to the bounding box spanned by the roi of imp. If no roi, does nothing.
+     * img must be the extracted single(!) channel of imp (see extractSingleChannelImg).
+     * @param img result of extractSingleChannelImg(imp, ...)
+     * @param imp 2D or 3D, single or multi-channel. single time point (multi-timepoint not tested!).
+     * @return interval: img cropped to the roi bounding rect, or not modified if no roi exists.
+     */
+    public static <T extends RealType<T>> RandomAccessibleInterval<T> restrictSingleChannelImgToRoiRect(RandomAccessibleInterval<T> img, ImagePlus imp) {
+        RandomAccessibleInterval<T> interval = img;
+
+        Roi roi = imp.getRoi();
+        if (roi != null) {
+            Rectangle bounds = roi.getBounds();
+            long[] istart = new long[]{bounds.x, bounds.y, 0, 0}; // xy(zt). single channel
+            long[] iend = new long[]{bounds.x + bounds.width, bounds.y + bounds.height,
+                    imp.getNSlices() - 1, imp.getNFrames() - 1}; // xy(z,t)
+
+            interval = Views.interval(img, istart, iend);
+        }
+
+        return interval;
+    }
 }
+
