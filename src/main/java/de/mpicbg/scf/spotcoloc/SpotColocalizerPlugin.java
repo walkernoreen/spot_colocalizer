491b9495b4466bbdd6ff3b738e392983dc8f4ec9
walker <walker@extern.mpi-cbg.de>
2020-06-18 16:00:12 +0200

    moved from nonblockinggenericdialog to scijava parameters. plugin type changed to interactivecommand

--- src/main/java/de/mpicbg/scf/spotcoloc/SpotColocalizerPlugin.java
+++ src/main/java/de/mpicbg/scf/spotcoloc/SpotColocalizerPlugin.java
@@ -4,7 +4,12 @@ package de.mpicbg.scf.spotcoloc;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.gui.*;
+import org.scijava.Cancelable;
+import org.scijava.ItemVisibility;
 import org.scijava.command.Command;
+import org.scijava.command.ContextCommand;
+import org.scijava.command.DynamicCommand;
+import org.scijava.command.InteractiveCommand;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.prefs.PrefService;
@@ -13,6 +18,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import org.scijava.widget.Button;
 
 
 /*
@@ -22,8 +28,8 @@ import java.awt.event.MouseListener;
 // ToDo s: also table with spots positions?
 // ToDo add help button?
 
-@Plugin(type = Command.class, menuPath = "Plugins>Spot Colocalization > SpotColoc") // TODO nicer path
-public class SpotColocalizerPlugin implements Command, MouseListener {
+@Plugin(type = InteractiveCommand.class, menuPath = "Plugins>Spot Colocalization > SpotColoc") // TODO nicer path
+public class SpotColocalizerPlugin extends InteractiveCommand  { //implements MouseListener {
 
     @Parameter
     private PrefService prefs;
@@ -31,260 +37,133 @@ public class SpotColocalizerPlugin implements Command, MouseListener {
     @Parameter
     ImagePlus imp;
 
-    Roi currentRoi;
-
-    // parameters (values are overwritten with previously stored values in ColocDialog)
+    // -- Dialog Parameters --
     // channel A
-    private int channelA=1;
+    @Parameter(label = "---  Channel A" , visibility = ItemVisibility.MESSAGE, persist = false, required=false)
+    private String m1 = " (spots displayed magenta) ---";
+
+    @Parameter(label="channel number")
+    private int channelA=2;
+
+    @Parameter(label="radius (um)")
     private double radiusA_um=1.0;
+
+    @Parameter(label="quality threshold")
     private double thresholdA=100.0;
-    // channel B
-    private int channelB=2;
+
+    //channel B
+    @Parameter(label = "---  Channel B" , visibility = ItemVisibility.MESSAGE, persist = false, required=false)
+    private String m2 = " (spots displayed green) ---";
+
+    @Parameter(label="channel number")
+    private int channelB=3;
+
+    @Parameter(label="radius (um)")
     private double radiusB_um=1.0;
+
+    @Parameter(label="quality threshold")
     private double thresholdB=100.0;
+
     // general
+    @Parameter(label = "---  General", visibility = ItemVisibility.MESSAGE, persist = false, required=false)
+    private String m3=" ---";
+
+    @Parameter(label="Coloc distance factor (default: 1)", description = "Spots are considered colocalized if their centers are closer than distance_factor*0.5*(radiusA+radiusB). factor=1: centers of spot pair are closer than their average radius.")
     private double distanceFactorColoc=1.0;
+
+    @Parameter(label="Include spots A in preview", persist = false)
     private boolean previewA=false;
+
+    @Parameter(label="Include spots B in preview", persist = false)
     private boolean previewB=false;
 
-    // TODO also add (do subpixel) and do median to the exposed options?
+    // processing buttons
+    @Parameter(label = "Generate Preview", callback="generatePreview_callback" )
+    private Button previewButton;
+
+    @Parameter(label = "Full Colocalization Analysis", callback="fullAnalysis_callback" )
+    private Button analysisButton;
+
+    // -- more private fields --
+    // TODO also add (do subpixel) and do median to the exposed options? (+rt display options?)
     final private boolean doSubixel=true;
     final private boolean doMedian=false;
 
-    // dialog
-    ColocDialog colocDialog;
+    Roi currentRoi;
 
     // spot analyzer
     private SpotColocalizer spotColocalizer;
 
 
+    /**
+     * Generates spots preview
+     */
+    // TODO: new thread? and IJ update with swingutilities?
+    private void generatePreview_callback() {
+        // reset image, grab roi
+        prepare();
+
+        System.out.println("Preview callback");
+        printParameters();
+
+        // show spot detection previews
+        if (checkParameters()) {
+            System.out.println("params ok. generating preview");
+            spotColocalizer.generateDetectionPreviewMultiChannel(previewA, previewB, channelA, radiusA_um,
+                    thresholdA, channelB, radiusB_um, thresholdB, doSubixel, doMedian);
+        }
+    }
+
+
+    /**
+     * Full colocalization analysis: detects spots and finds colocalized spots. Creates a results table.
+     */
+    private void fullAnalysis_callback() {
+        // reset image, grab roi
+        prepare();
+
+        printParameters();
+
+        // do spot detection + colocalization. displays results table
+        if (checkParameters()) {
+            System.out.println("params ok. running full analysis.");
+
+            spotColocalizer.runFullColocalizationAnalysis(channelA, radiusA_um, thresholdA,
+                    channelB, radiusB_um, thresholdB, distanceFactorColoc,
+                    doSubixel, doMedian, true); // TODO: cleartable? -> expose this option?
+            // ToDo make an option that returns the results?
+        } else {
+            IJ.error("Parameter error in Spot Colocalization",
+                    "Some parameters were not ok. Not running plugin.\n" +
+                            "See Console Window for current parameters.");
+        }
+    }
+
+
+    /**
+     * Helper to update data before generating preview or doing full analysis
+     */
+    private void prepare() {
+        spotColocalizer=new SpotColocalizer(imp);             //TODO: initialie only once?
+        currentRoi=imp.getRoi();
+        imp.setOverlay(null);
+    }
+
+
+    @Override
+    public void preview() {
+        super.preview();
+        System.out.println("Previewing - but doing nothing (but saving params)");
+    }
+
+
     @Override
     public void run() {
-
-        // load stored parameters from preferences file
-        loadStoredParameters();
-
-        // new analyzer
-        spotColocalizer= new SpotColocalizer(imp);
-
-        currentRoi=imp.getRoi();
-
-        imp.setOverlay(null);
-
-        // add mouse listener -> tracks drawing of Rois
-        ImageCanvas canvas = imp.getWindow().getCanvas();
-        canvas.addMouseListener(this); // prepend with this?:canvas.removeMouseListener(IJ.getInstance()); ?
-
-        // the main processing: dialog for preview + full analysis
-        colocDialog = new ColocDialog();
-        boolean success = colocDialog.displayAndRun();
-
-        // store parameter values
-        if (success) {
-            storeParameters();
-        }
-    }
-
-
-    /**
-     * Plugin dialog
-     */
-    private class ColocDialog implements DialogListener{
-
-        NonBlockingGenericDialog gd;
-
-        /**
-         * Initialize dialog and sets up dialog fields
-         */
-        ColocDialog() {
-            //set up dialog
-            gd = new NonBlockingGenericDialog("Spot Colocalization");
-
-            // channel A
-            gd.addMessage("Channel A (spots displayed magenta)");
-            gd.addNumericField("channel number",channelA,0,10,""); // 0,10,"": digits,ncolumns, unit
-            gd.addNumericField("radius (um)", radiusA_um, 2, 10, "");
-            gd.addNumericField("quality threshold", thresholdA, 2, 10, "");
-            // channel B
-            gd.addMessage("Channel B (spots displayed green)");
-            gd.addNumericField("channel number", channelA, 0, 10, "");
-            gd.addNumericField("radius (um)", radiusA_um, 2, 10, "");
-            gd.addNumericField("quality threshold", thresholdB, 2, 10, "");
-            // general
-            gd.addMessage("General");
-            gd.addNumericField("Coloc distance factor (default: 1)",distanceFactorColoc,1); // factor of average radius.
-            gd.addCheckbox("preview spots in channel A",false);
-            gd.addCheckbox("preview spots in channel B",false);
-            // preview button
-            // combined from https://forum.image.sc/t/genericdialog-window-prevent-interaction-with-other-windows/12067/4
-            // and https://github.com/imagej/ImageJA/blob/230e425c01d6e17f88cb770113c2f5119330141f/src/main/java/ij/gui/GenericDialog.java#L813
-            Button previewButton= new Button("Generate Preview");
-            GridBagConstraints constraints = new GridBagConstraints();
-            constraints.gridy=GridBagConstraints.RELATIVE;
-            constraints.gridx=0;
-            constraints.insets = new Insets(10,20,0,0);
-            gd.add(previewButton,constraints);
-            gd.addMessage(""); // dummy spacer since our gd doesn't know about the button location
-
-
-        }
-
-
-        /**
-         * Displays dialog, provides previewing functionaliy and does full processing pipeline
-         * preview (preview checkboxes): show spot detection
-         * full analysis (ok button): spot detection + colocalization
-         * @return whether run finished successfully
-         */
-        private boolean displayAndRun() {
-            // listen to changes: generates spots preview
-            gd.addDialogListener(this);
-
-            gd.showDialog();
-
-            // if dialog was canceled
-            if (gd.wasCanceled()) {
-                return false;
-            }
-
-            // dialog was ok'd: full processing
-            if (gd.wasOKed()) {
-                printParameters();
-
-                // do spot detection + colocalization. displays results table
-                if (checkParameters()) {
-                    System.out.println("running full analysis.");
-
-                    spotColocalizer.runFullColocalizationAnalysis(channelA, radiusA_um, thresholdA,
-                            channelB, radiusB_um, thresholdB, distanceFactorColoc,
-                            doSubixel, doMedian, true); // TODO: cleartable? -> expose this option?
-
-                    return true;
-                }
-                else {
-                    IJ.error("Parameter error in Spot Colocalization",
-                            "Some parameters were not ok. Not running plugin.\n" +
-                                    "See Console Window for current parameters.");
-                    return false;
-                }
-            }
-            return false; // shouldn't be reached
+            System.out.println("Running - but doing nothing (but saving params)");
         }
 
 
 
-        /**
-         * Generate spots preview. This function also extracts the parameters from the dialog.
-         * Important: Do not extract parameters at any other code location (i.e. not after
-         * Dialog was Ok'd or MouseListener reported changes (would try to access fields beyond the
-         * number of actually existing fields in dialog (since counter keeps increasing).
-         * @param genericDialog which genericDialog is this?
-         * @param awtEvent
-         * @return
-         */
-        @Override
-        public boolean dialogItemChanged(GenericDialog genericDialog, AWTEvent awtEvent) {
-            //TODO for future: only call the spot detector if anything changed compared to previous variables?
-
-            System.out.println("Nested Dialog item changed at: "+java.time.LocalTime.now().toString());
-
-            // extract parameters from dialog
-            // channel A
-            channelA= (int) gd.getNextNumber();
-            radiusA_um = gd.getNextNumber();
-            thresholdA = gd.getNextNumber();
-            // channel B
-            channelB = (int) gd.getNextNumber();
-            radiusB_um=gd.getNextNumber();
-            thresholdB = gd.getNextNumber();
-            //general
-            distanceFactorColoc=gd.getNextNumber();
-            previewA=gd.getNextBoolean();
-            previewB=gd.getNextBoolean();
-
-
-            printParameters();
-
-            // show spot detection previews
-            if (checkParameters()) {
-                System.out.println("preview call triggered");
-                spotColocalizer.generateDetectionPreviewMultiChannel(previewA, previewB, channelA, radiusA_um,
-                        thresholdA, channelB, radiusB_um, thresholdB, doSubixel, doMedian);
-            }
-
-            return true; // keep OK button activated
-        }
-    }
-
-
-    /**
-     * Checks whether a ROI was updated/removed
-     * @param e
-     */
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        Roi roi = imp.getRoi();
-        if (roi==null) {
-            if (currentRoi!=null) {
-                System.out.println("Roi was removed. at: "+java.time.LocalTime.now().toString());
-
-                // generate spot detections preview
-                printParameters();
-                if (checkParameters()) {
-                    System.out.println("preview call triggered");
-                    spotColocalizer.generateDetectionPreviewMultiChannel(previewA, previewB, channelA, radiusA_um,
-                            thresholdA, channelB, radiusB_um, thresholdB, doSubixel, doMedian);
-                }
-
-            }
-            currentRoi=null;
-        }
-        else {
-            boolean isFinished = (roi.getState()==Roi.NORMAL);
-            boolean isDifferent = !roi.equals(currentRoi);
-
-            if (isFinished && isDifferent) {
-                System.out.println("Roi was modified. at: "+java.time.LocalTime.now().toString());
-
-                // generate spot detections preview
-                printParameters();
-                if (checkParameters()) {
-                    System.out.println("preview call triggered");
-                    spotColocalizer.generateDetectionPreviewMultiChannel(previewA, previewB, channelA, radiusA_um,
-                            thresholdA, channelB, radiusB_um, thresholdB, doSubixel, doMedian);
-                }
-            }
-            currentRoi= (Roi) roi.clone();
-        }
-    }
-
-
-    /**
-     * Loads stored parameter values from disk (from preferences file).
-     * If parameter is not found, uses default values.
-     */
-    private void loadStoredParameters() { // TODO somewhere the parameters are not stored or read correctly (same for A&B)
-        channelA = prefs.getInt(this.getClass(), "channelA", channelA);
-        radiusA_um = prefs.getDouble(this.getClass(),"radiusA_um",radiusA_um);
-        thresholdA=prefs.getDouble(this.getClass(),"thresholdA",thresholdA);
-        channelB = prefs.getInt(this.getClass(), "channelB", channelB);
-        radiusB_um = prefs.getDouble(this.getClass(),"radiusB_um",radiusB_um);
-        thresholdB=prefs.getDouble(this.getClass(),"thresholdB",thresholdB);
-        distanceFactorColoc=prefs.getDouble(this.getClass(),"distanceFactorColoc",distanceFactorColoc);
-    }
-
-    /**
-     * Saves parameter values to disk into the preferences file.
-     */
-    private void storeParameters() {
-        prefs.put(this.getClass(), "channelA", channelA);
-        prefs.put(this.getClass(), "radiusA_um", radiusA_um);
-        prefs.put(this.getClass(), "thresholdA", thresholdA);
-        prefs.put(this.getClass(), "channelB", channelB);
-        prefs.put(this.getClass(), "radiusB_um", radiusB_um);
-        prefs.put(this.getClass(), "thresholdB", thresholdB);
-        prefs.put(this.getClass(), "distanceFactorColoc", distanceFactorColoc);
-    }
 
     /**
      * Checks that inputs are not NaN and that neither channel nor radius is zero.
@@ -312,25 +191,5 @@ public class SpotColocalizerPlugin implements Command, MouseListener {
         System.out.print("General: ");
         System.out.println("distanceFactorColoc="+distanceFactorColoc+", previewA="+previewA+", previewB="+previewB);
     }
-
-
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-    }
-
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-    }
-
 
 }
